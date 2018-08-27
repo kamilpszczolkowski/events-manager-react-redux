8ebd6b7cf66078e07293982a7076a210bce83f6d
K P <kamilpszczola@gmail.com>
2018-08-27 20:20:40 +0200

    Added routing in App.js
    Necessary imports were made from react-router-dom (HashRouter and
    Route) to implement Routing in project. URL will define components
    displayed on the screen:
    * default path - SearchPage,
    * /events - BrowseEvents,
    * /events/id - SingleEvent (with specific id),
    * /addevents - AddEvent,
    * /map - EventMap.
    
    All containers were imported.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { HashRouter, Route } from "react-router-dom";
+import {
+  AddEvent,
+  SingleEvent,
+  EventMap,
+  BrowseEvents,
+  SearchPage
+} from "./containers";
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>App component</p>
-      </div>
+      <HashRouter>
+        <div>
+          <Route exact path="/" component={SearchPage} />
+          <Route exact path="/events" component={BrowseEvents} />
+          <Route path="/events/:id" component={SingleEvent} />
+          <Route path="/addevents" component={AddEvent} />
+          <Route path="/map" component={EventMap} />
+        </div>
+      </HashRouter>
     );
   }
 }
