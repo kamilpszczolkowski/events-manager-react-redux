1ab7a162b9cb33021039baaeea2fce4115c99ac7
K P <kamilpszczola@gmail.com>
2018-08-27 12:25:07 +0200

    Project initialisation
    Recreating development environment based on my previous project
    'event_manager':
    (https://github.com/kamilpszczolkowski/events_manager).
    Webpack config includes babel-loader, sass compiler, file-loader and
    html-webpack-plugin.
    Source code will be put in src folder. Index.js is a base js file in
    project.
    Images and scss folders were copied from previous project. For now I
    will use my old way of implementing styles to the page - it's only
    because I already did that, later I'll try to separate scss to the
    smaller files corresponding to their components.
    Gitignore and README.md files were added.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# events-manager-react-redux
+# Events-manager-react-redux
 Application allowing users to browse through events and to manage them. Site is built in JavaScript React and Redux, using Firebase realtime database for persistent data storage and google maps/places API for easy events locations marking.
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+var path = require('path');
+var Html = require('html-webpack-plugin');
+var MiniCSS = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "./js/out.js",
+        path: path.resolve(__dirname, "docs"),
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.(jsx|js)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            'env',
+                            'stage-2',
+                            'react'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                     MiniCSS.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png|csv)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        publicPath: 'images',
+                        outputPath: 'images'
+                    }
+                }
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        publicPath: 'fonts',
+                        outputPath: 'fonts'
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new Html({
+            filename: 'index.html',
+            template: './index.html',
+        }),
+        new MiniCSS({
+            filename: "app.css",
+        })
+    ]
+}
