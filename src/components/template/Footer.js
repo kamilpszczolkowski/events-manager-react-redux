4ea0a1e7d6d4b36e9e0f28e59ad46a878a0a843e
K P <kamilpszczola@gmail.com>
2018-08-27 22:56:34 +0200

    Defined website Footer
    First version of footer not separated to smaller components, consisting
    of the text section and small buttons, which are the navigation buttons
    for external pages. There is unused activeSite prop - it will be used in
    a future.

--- src/components/template/Footer.js
+++ src/components/template/Footer.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Footer = props => {
-  return <h2>Placeholder: Footer</h2>;
+const Footer = ({ activeSite }) => {
+  return <footer>
+      <div className="container">
+        <strong className="footerTitle">Kamil Pszczółkowski @2018</strong>
+        <span className="footerInfo">
+          Aplikacja do zarządzania wydarzeniami{" "}
+        </span>
+        <div className="buttons">
+          <a href="https://www.linkedin.com/in/kamil-pszcz%C3%B3%C5%82kowski-9b9894166/">
+            <div className="btn2" />
+          </a>
+          <a href="mailto:pszczolkowski.kamil@gmail.com">
+            <div className="btn3" />
+          </a>
+          <a href="https://github.com/kamilpszczolkowski">
+            <div className="btn4" />
+          </a>
+        </div>
+      </div>
+    </footer>;
 };
 
 export default Footer;
