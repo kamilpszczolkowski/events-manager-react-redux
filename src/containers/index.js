6494ca18694738202f8f8f88582c90f6af1eee18
K P <kamilpszczola@gmail.com>
2018-08-27 20:04:10 +0200

    New positions in index.jsin containers folder
    Default imports of AddEvent.js, BrowseEvent.js, EventMap.js and
    SingleEvent.js were added.

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,1 +1,5 @@
+export { default as AddEvent } from "./AddEvent";
+export { default as BrowseEvents } from "./BrowseEvents";
+export { default as EventMap } from "./EventMap";
 export { default as SearchPage } from "./SearchPage";
+export { default as SingleEvent } from "./SingleEvent";
