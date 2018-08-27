964f0ef7e1c1973b757733866e3fb7707d231f72
K P <kamilpszczola@gmail.com>
2018-08-27 13:43:04 +0200

    Creating react/redux file structure and redux store.
    New folders were created in src folder:
    * containers - for components communicating with redux store,
    * components - for all other, smaller components,
    * services - for external API services,
    * store - files related to the redux store (action creators and
    reducers).
    Template file reducers.js was created. Redux store was defined in
    index.js with reducers and MiddleWare - thunk (it will be necessary
    later).

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './App';
-
 import styles from '../scss/style.scss';
 
+import * as reducers from './store/reducers';
+
+const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
+
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
