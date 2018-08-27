80234aa85ef597e1ad83cb6abd4670b58431bdfa
K P <kamilpszczola@gmail.com>
2018-08-27 21:48:45 +0200

    Created boilerplate for template components
    Template includes React import with functional component and default
    export of component.
    Boilerplates created for: Footer.js, Header.js, Template.js.

---
+++ src/components/template/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = props => {
+  return <h2>Placeholder: Footer</h2>;
+};
+
+export default Footer;
---
+++ src/components/template/Template.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Template = (props) => {
+  return <h2>Placeholder: Template</h2>;
+};
+
+export default Template;
