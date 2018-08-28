aa04ea731d37d2fdc18cad8865b344bf57aea34e
K P <kamilpszczola@gmail.com>
2018-08-28 17:09:57 +0200

    Added Search filed in BrowseEvents.js
    Search bar was added as common component InputField.js. Selector from
    filters section was used (getCurrentSearchText) and action creator
    called serachTextChanged were passed as props and handled there.

--- src/containers/BrowseEvents.js
+++ src/containers/BrowseEvents.js
@@ -1,26 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Template } from "../components/template";
+import * as filtersSelectors from "../store/filters/reducer";
+import { searchTextChanged } from "../store/filters/actions";
+import { InputField } from "../components/common";
 
 class BrowseEvents extends Component {
   render() {
     return (
       <Template activeSite="browseEvents">
-
+          <InputField
+            placeholder="Czego szukasz?"
+            id="searchInevents"
+            value={this.props.searchText}
+            onChangeFunc={this.props.searchTextChanged}
+          />
       </Template>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return {};
+  return { searchText: filtersSelectors.getCurrentSearchText(state) };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {};
+function mapDispatchToProps() {
+  return { searchTextChanged };
 }
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps()
 )(BrowseEvents);
