04619754d4b8aa06629d5fb1d2f70789dd80584d
K P <kamilpszczola@gmail.com>
2018-08-28 11:16:14 +0200

    Separated Footer to smaller components
    Buttons section of the footer was moved to different component named
    FooterIcons.js.

--- src/components/template/Footer.js
+++ src/components/template/Footer.js
@@ -1,25 +1,18 @@
 import React from "react";
+import FooterIcons from "./FooterIcons";
 
 const Footer = ({ activeSite }) => {
-  return <footer>
+  return (
+    <footer>
       <div className="container">
         <strong className="footerTitle">Kamil Pszczółkowski @2018</strong>
         <span className="footerInfo">
           Aplikacja do zarządzania wydarzeniami{" "}
         </span>
-        <div className="buttons">
-          <a href="https://www.linkedin.com/in/kamil-pszcz%C3%B3%C5%82kowski-9b9894166/">
-            <div className="btn2" />
-          </a>
-          <a href="mailto:pszczolkowski.kamil@gmail.com">
-            <div className="btn3" />
-          </a>
-          <a href="https://github.com/kamilpszczolkowski">
-            <div className="btn4" />
-          </a>
-        </div>
+        <FooterIcons />
       </div>
-    </footer>;
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ src/components/template/FooterIcons.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const FooterIcons = () => {
+  return (
+    <div className="buttons">
+      <a href="https://www.linkedin.com/in/kamil-pszcz%C3%B3%C5%82kowski-9b9894166/">
+        <div className="btn2" />
+      </a>
+      <a href="mailto:pszczolkowski.kamil@gmail.com">
+        <div className="btn3" />
+      </a>
+      <a href="https://github.com/kamilpszczolkowski">
+        <div className="btn4" />
+      </a>
+    </div>
+  );
+};
+
+export default FooterIcons;
