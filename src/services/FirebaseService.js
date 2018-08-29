b90349cebaab293b473bd64949d8a4209df90c8e
K P <kamilpszczola@gmail.com>
2018-08-29 15:17:36 +0200

    Added to firebase service getEvents id parameter
    Id parameter is optional - when added - service will fetch only one
    specific event corresponding to the id passed by parameter.

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -2,8 +2,13 @@ const FIREBASE_ENDPOINT =
   "https://wydarzenia-b6ffc.firebaseio.com/lista_wydarzen";
 
 export default class FirebaseService {
-  static getEvents = callback => {
-    const url = `${FIREBASE_ENDPOINT}.json`;
+  static getEvents = (callback, id = "") => {
+    let url;
+    if (id === "") {
+      url = `${FIREBASE_ENDPOINT}.json`;
+    } else {
+      url = `${FIREBASE_ENDPOINT}/${id}.json`;
+    }
     fetch(url)
       .then(response => {
         if (response.ok) {
