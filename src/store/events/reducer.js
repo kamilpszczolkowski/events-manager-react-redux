a09de1dc5b74bcd983adfee93df61e35b7e1ee89
K P <kamilpszczola@gmail.com>
2018-08-29 11:51:58 +0200

    Created selector getEventsFetched
    Selector takes eventsFetched object from the store and converts it to
    the array. Array consists of events as object with added id as a
    previous key value.
    In next step array is sorted by the selected sort method from the store.
    At the end array is filtered by the search prhase from the store.
    Sorted and filtered array is returned by the function.

--- src/store/events/reducer.js
+++ src/store/events/reducer.js
@@ -1,14 +1,77 @@
 import * as types from "./actionTypes";
 
 const initialState = {
-  events: {}
+  eventsFetched: {}
 };
 
 export default function reduce(state = initialState, action = {}) {
   switch (action.type) {
     case types.EVENTS_FETCHED:
-      return { ...state, events: action.events };
+      return { ...state, eventsFetched: action.events };
     default:
       return state;
   }
+}
+
+export function getEventsFetched(state) {
+  const { eventsFetched } = state.events;
+  const eventsArray = [];
+  let sortedArray = [],
+    filteredArray = [];
+  //Transfrom object with evnets as objects to array with events as objects
+  for (let key in eventsFetched) {
+    const newElement = { id: key };
+    for (let newKey in eventsFetched[key]) {
+      newElement[newKey] = eventsFetched[key][newKey];
+    }
+    eventsArray.push(newElement);
+  }
+  //Sort array by chosen sorting criteria
+  switch (state.filters.sortMethod) {
+    case "namesAsc":
+      sortedArray = eventsArray.sort((a, b) => {
+        if (a.name < b.name) return 1;
+        if (a.name > b.name) return -1;
+        return 0;
+      });
+      break;
+    case "namesDesc":
+      sortedArray = eventsArray.sort((a, b) => {
+        if (a.name > b.name) return 1;
+        if (a.name < b.name) return -1;
+        return 0;
+      });
+      break;
+    case "datesAsc":
+      sortedArray = eventsArray.sort((a, b) => {
+        if (a.date < b.date) return 1;
+        if (a.date > b.date) return -1;
+        return 0;
+      });
+      break;
+    case "datesDesc":
+      sortedArray = eventsArray.sort((a, b) => {
+        if (a.date > b.date) return 1;
+        if (a.date < b.date) return -1;
+        return 0;
+      });
+      break;
+    default:
+      sortedArray = eventsArray;
+  }
+
+  //Filter events by search phrase
+  const searchPhraseLoCase = state.filters.searchText.toLowerCase();
+  if (searchPhraseLoCase !== "") {
+    filteredArray = sortedArray.filter(el => {
+      if (el.name.toLowerCase().indexOf(searchPhraseLoCase) !== -1) return true;
+      if (el.descShort.toLowerCase().indexOf(searchPhraseLoCase) !== -1)
+        return true;
+      if (el.type.toLowerCase().indexOf(searchPhraseLoCase) !== -1) return true;
+      if (el.place.toLowerCase().indexOf(searchPhraseLoCase) !== -1)
+        return true;
+      return false;
+    });
+  } else filteredArray = sortedArray;
+  return filteredArray;
 }
