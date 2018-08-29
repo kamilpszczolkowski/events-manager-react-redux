f1d48725d0d12b7d70ead3947bca57ef83a00234
K P <kamilpszczola@gmail.com>
2018-08-29 15:24:01 +0200

    Combined reducer from eventForm with other reducers
    Reducer from eventForm was imported in src/store/reducers.js file - it
    was combined in export position.

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,4 +1,5 @@
 import filters from "./filters/reducer";
 import events from "./events/reducer";
+import eventForm from "./eventForm/reducer";
 
-export { filters, events };
+export { filters, events, eventForm };
