c1568e0b5abff0b9c5a296eb4e9c377b767e8dc3
K P <kamilpszczola@gmail.com>
2018-08-29 20:17:40 +0200

    Updated index.js with new exports
    Positions addded:
    * EventImageSection,
    * EventDescription,
    * EventDataButtons.

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,6 @@ export { default as SearchPageBackground } from "./SearchPageBackground";
 export { default as SearchField } from "./SearchField";
 export { default as SortTab } from "./SortTab";
 export { default as EventTab } from "./EventTab";
+export { default as EventImageSection } from "./EventImageSection";
+export { default as EventDescription } from "./EventDescription";
+export { default as EventDataButtons } from "./EventDataButttons";
