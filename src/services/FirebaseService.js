5d2347cc23efcc4a1a2f9ca8007927695ef1d230
K P <kamilpszczola@gmail.com>
2018-08-29 20:02:19 +0200

    Added Change event and delete event function in firebase
    ChangeEvent require callback, data and id - callback will be
    actioncreator which confrims that event has been updated. Data is one
    single event formated in extact the same way as in the server.
    
    Function deleteEvent requieres callback and id. Callback will be action
    creator confirming that event has been deleted.

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -25,6 +25,54 @@ export default class FirebaseService {
         } else {
           throw new Error("Callback isn't a function");
         }
-      });
+      })
+      .catch(err => console.log(err));
+  };
+  static changeEvent = (callback, data, id) => {
+    const url = `${FIREBASE_ENDPOINT}/${id}.json`;
+    fetch(url, {
+      method: "PATCH",
+      body: JSON.stringify(data)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(
+            "Network error - can't connect to the Firebase Server"
+          );
+        }
+      })
+      .then(jsonResponse => {
+        if (typeof callback === "function") {
+          callback(jsonResponse);
+        } else {
+          throw new Error("Callback isn't a function");
+        }
+      })
+      .catch(err => console.log(err));
+  };
+  static deleteEvent = (callback, id) => {
+    const url = `${FIREBASE_ENDPOINT}/${id}.json`;
+    fetch(url, {
+      method: "DELETE"
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(
+            "Network error - can't connect to the Firebase Server"
+          );
+        }
+      })
+      .then(jsonResponse => {
+        if (typeof callback === "function") {
+          callback(jsonResponse);
+        } else {
+          throw new Error("Callback isn't a function");
+        }
+      })
+      .catch(err => console.log(err));
   };
 }
