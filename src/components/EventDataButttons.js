2bb7a06fc8a1a58baf9d47ec6c4f91578b7e3330
K P <kamilpszczola@gmail.com>
2018-08-30 14:31:56 +0200

    Removed editing mode from EventDataButtons.js
    All event editing functions and parameters were removed from this
    component.

--- src/components/EventDataButttons.js
+++ src/components/EventDataButttons.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 
 export default class EventDataButttons extends Component {
-  classNameEditMode() {
-    if (this.props.editingMode) {
-      return "editionMode";
-    }
-    return "";
-  }
-
   static dateExtract(date, place) {
     if (place === "begin") {
       return date.slice(0, 10);
@@ -15,42 +8,22 @@ export default class EventDataButttons extends Component {
     return date.slice(11, 16);
   }
 
-  displayDateshelperFunc(date) {
-    if (this.props.editingMode) {
-      return <input id="dateStartEdit" type="datetime-local" />;
-    }
-    return (
-      <p>
-        {EventDataButttons.dateExtract(date, "begin")},{" "}
-        {EventDataButttons.dateExtract(date, "end")}
-      </p>
-    );
-  }
-
   render() {
-    const classNames = this.classNameEditMode();
     const { place, date, type } = this.props.event;
-    const { editingMode } = this.props;
-
     return (
       <div className="singleEventcol2">
         <h2>Miejsce</h2>
         <p id="editedPlace">{place}</p>
         <h2>Rozpoczęcie</h2>
-        {this.displayDateshelperFunc(date[0])}
+          {EventDataButttons.dateExtract(date[0], "begin")},{" "}
+          {EventDataButttons.dateExtract(date[0], "end")}
         <h2>Zakończenie</h2>
-        <p
-          id="editedDateEnd"
-          className={classNames}
-          contentEditable={editingMode}
-        >
+        <p id="editedDateEnd">
           {EventDataButttons.dateExtract(date[1], "begin")},{" "}
           {EventDataButttons.dateExtract(date[1], "end")}
         </p>
         <h2>Typ wydarzenia</h2>
-        <p id="editedType" className={classNames} contentEditable={editingMode}>
-          {type}
-        </p>
+        <p id="editedType">{type}</p>
       </div>
     );
   }
