ec283f2cd497927d2dad1f88cc88682055d15cc9
K P <kamilpszczola@gmail.com>
2018-08-30 10:11:44 +0200

    added createEvent in FirebaseService
    createEvent allows to send new event data to the firebase server with
    "POST" method - server will create it with new, unique id.

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -75,4 +75,28 @@ export default class FirebaseService {
       })
       .catch(err => console.log(err));
   };
+  static createEvent = (callback, data) => {
+    const url = `${FIREBASE_ENDPOINT}.json`;
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(
+            "Network error - can't connect to the Firebase Server"
+          );
+        }
+      })
+      .then(jsonResponse => {
+        if (typeof callback === "function") {
+          callback(jsonResponse);
+        } else {
+          throw new Error("Callback isn't a function");
+        }
+      })
+      .catch(err => console.log(err));
+  };
 }
