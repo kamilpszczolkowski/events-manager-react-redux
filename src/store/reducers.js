f5f10631ffdfac6188d6376466e00924f03aa4f2
K P <kamilpszczola@gmail.com>
2018-08-30 11:46:11 +0200

    event Form reducer exported into combined reducers
    File reducer.js is included in imports/exports of reducers.js file,
    which exports all reducers to the store.

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,6 @@
 import filters from "./filters/reducer";
 import events from "./events/reducer";
 import singleEvent from "./singleEvent/reducer";
+import eventForm from "./eventForm/reducer"
 
-export { filters, events, singleEvent };
+export { filters, events, singleEvent, eventForm };
