5caf44308927c31e6a8d4a32ea7def47448de690
K P <kamilpszczola@gmail.com>
2018-08-30 09:47:01 +0200

    Added new components to SingleEvent.js
    New components were added:
    * EventImageSection,
    * EventDescription,
    * EventDataButtons.
    
    Components show specific data for single event. Components are rendered,
    if the event was fetched from the firebase server.

--- src/containers/SingleEvent.js
+++ src/containers/SingleEvent.js
@@ -1,26 +1,64 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Template } from "../components/template";
+import {
+  EventImageSection,
+  EventDescription,
+  EventDataButtons
+} from "../components";
+import { eventFetched } from "../store/eventForm/actions";
+import {
+  getEventFetched,
+  checkIfEventIsEdited
+} from "../store/eventForm/reducer";
+import EventDataButttons from "../components/EventDataButttons";
 
 class SingleEvent extends Component {
+  componentDidMount() {
+    this.props.eventFetched(this.props.match.params.id);
+  }
+
+  eventRenderHelperFunc() {
+    if ( Object.keys(this.props.event).length === 0) return <h2>Wczytywanie</h2>;
+    return (
+      <div>
+        <EventImageSection
+          imgSrc={this.props.event.img}
+          editingMode={this.props.eventEdited}
+        />
+        <EventDescription
+          editingMode={this.props.eventEdited}
+          event={this.props.event}
+        />
+        <EventDataButttons
+          editingMode={this.props.eventEdited}
+          event={this.props.event}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <Template activeSite="singleEvent">
-        <h2>Single event page</h2>
+          {this.eventRenderHelperFunc()}
       </Template>
     );
   }
 }
 
 function mapStateToProps(state) {
-  return {};
+  return {
+    event: getEventFetched(state),
+    eventEdited: checkIfEventIsEdited(state)
+  };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {};
+function mapDispatchToProps() {
+  return { eventFetched };
 }
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps()
 )(SingleEvent);
