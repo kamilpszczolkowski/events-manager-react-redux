4603dc22161552b6ac076851d24b89d4ac798d25
K P <kamilpszczola@gmail.com>
2018-08-30 14:26:26 +0200

    Removed event editing from EventDescription.js
    All editing functions/parameters were removed from this component. It's
    type was changed from class type component to the functional component.

--- src/components/EventDescription.js
+++ src/components/EventDescription.js
@@ -1,62 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class EventDescription extends Component {
-  classNameEditMode() {
-    if (this.props.editingMode) {
-      return "editionMode";
-    }
-    return "";
-  }
+const EventDescription = ({ event }) => {
+  const { name, organisation, desc } = event;
+  return (
+    <div className="singleEventcol1">
+      <section className="signleElTitle">
+        <h2 id="editedName">{name}</h2>
+        <h3 id="editedOrg">
+          <i>{organisation}</i>
+        </h3>
+      </section>
+      <p id="editedDesc">{desc}</p>
+    </div>
+  );
+};
 
-  shortDescriptionEdit() {
-    if (this.props.editingMode) {
-      return (
-        <div>
-          <p>Opis skrócony</p>
-          <p
-            id="editedDescShort"
-            className="editionMode"
-            contentEditable="true"
-          >
-            {this.props.event.descShort}
-          </p>
-        </div>
-      );
-    }
-    return null;
-  }
-
-  render() {
-    const classNames = this.classNameEditMode();
-    const { editingMode } = this.props;
-    const { name, organisation, desc } = this.props.event;
-    return (
-      <div className="singleEventcol1">
-        <section className="signleElTitle">
-          <h2
-            id="editedName"
-            className={classNames}
-            contentEditable={editingMode}
-          >
-            {name}
-          </h2>
-          <h3
-            id="editedOrg"
-            className={classNames}
-            contentEditable={editingMode}
-          >
-            <i>{organisation}</i>
-          </h3>
-        </section>
-        <p
-          id="editedDesc"
-          className={editingMode ? "editionMode" : "singleElDesc"}
-          contentEditable={editingMode}
-        >
-          {desc}
-        </p>
-        {this.shortDescriptionEdit()}
-      </div>
-    );
-  }
-}
+export default EventDescription;
