13dee2df364beadb6c4c7470493075b82a6f67b4
K P <kamilpszczola@gmail.com>
2018-08-30 19:01:18 +0200

    Created Popup confirming finising of event editing
    Component informs about successfull event editing with button
    redirecting to the event page. If user desn't click on it - redirection
    will happen automatically after 3 seconds.

---
+++ src/components/EditedEventConfirm.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class EditedEventConfirm extends Component {
+  eventEditedHelperFunc() {
+    const { editingFinished, editingEvent } = this.props;
+
+    if (editingFinished) {
+      this.timeout = setTimeout(() => {
+        window.location.href = "#/events/" + editingEvent;
+      }, 3000);
+
+      return (
+        <div className="EventDeleted">
+          <section>
+            <h2>Zmiany zostały zapisane</h2>
+            <p>Nastąpi przekierowanie do strony wydarzenia</p>
+            <button>
+              <Link to={"#/events/" + editingEvent}>Idź do wydarzenia</Link>
+            </button>
+          </section>
+        </div>
+      );
+    }
+    return null;
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
+
+  render() {
+    return this.eventEditedHelperFunc();
+  }
+}
