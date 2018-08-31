1d671e0a8857fbae72d84e254042b1842f1790c2
K P <kamilpszczola@gmail.com>
2018-08-31 10:50:05 +0200

    Small code refactoring in EventMapWrapper
    Code has been refactored in this component, functionality wasn't
    changed.

--- src/components/googleMaps/EventMapWrapper.js
+++ src/components/googleMaps/EventMapWrapper.js
@@ -2,19 +2,16 @@ import React, { Component } from "react";
 import { GoogleApiWrapper } from "google-maps-react";
 import EventMap from "./EventMap";
 
-class TempApp2 extends Component {
+class EventMapApp extends Component {
   render() {
+    const { google, googleId, googlePos } = this.props;
     return (
       <div>
-        <EventMap
-          google={this.props.google}
-          googleId={this.props.googleId}
-          googlePos={this.props.googlePos}
-        />
+        <EventMap google={google} googleId={googleId} googlePos={googlePos} />
       </div>
     );
   }
 }
 export default GoogleApiWrapper({
   apiKey: "AIzaSyAF0dMnU6SE60S3AJT1t7UpWTbVgZI_5CQ"
-})(TempApp2);
+})(EventMapApp);
